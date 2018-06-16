c6ab4573a40b4d42a7ee6233abdc748bff3d9b47
ValteirEleuterio <valteir.ea@hotmail.com>
2018-06-16 09:09:37 -0300

    Arrays Exercicio 01

--- uds-trainee-aula-2/src/com/company/Main.java
+++ uds-trainee-aula-2/src/com/company/Main.java
@@ -6,20 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> dadosPessoais = new ArrayList();
+        ArrayList<String> cadastrosDados = new ArrayList();
+        cadastrosDados.add("Nome: ");
+        cadastrosDados.add("Cep: ");
+        cadastrosDados.add("Rua: ");
+        cadastrosDados.add("Numero: ");
+        cadastrosDados.add("Complemento: ");
+        cadastrosDados.add("Bairro: ");
+        cadastrosDados.add("Cidade: ");
+        cadastrosDados.add("Estado: ");
+        cadastrosDados.add("Telefone: ");
+
+
         dadosPessoais.add("Valteir");
         dadosPessoais.add("999-999");
         dadosPessoais.add("Rua das Palmeiras");
-        dadosPessoais.add("nº 325");
+        dadosPessoais.add("325");
         dadosPessoais.add("Casa");
         dadosPessoais.add("Vila Matinhos");
         dadosPessoais.add("Maringá");
         dadosPessoais.add("Paraná");
         dadosPessoais.add("(44)99812-5460");
 
-        dadosPessoais.set(0,"Valteir Eleuterio Alves");
+        dadosPessoais.set(0, "Valteir Eleuterio Alves");
         dadosPessoais.remove(8);
 
-        System.out.println(dadosPessoais);
-
+        for (int i = 0; i < dadosPessoais.size(); i++) {
+            System.out.println(cadastrosDados.get(i) + dadosPessoais.get(i));
+        }
     }
 }
+
+
