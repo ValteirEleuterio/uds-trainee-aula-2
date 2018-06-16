fe79016dd7e49bca5945ca0092cbc22c41281594
ValteirEleuterio <valteir.ea@hotmail.com>
2018-06-16 09:24:49 -0300

    percorrer array com forEach

--- uds-trainee-aula-2/src/com/company/Main.java
+++ uds-trainee-aula-2/src/com/company/Main.java
@@ -6,17 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> dadosPessoais = new ArrayList();
-        ArrayList<String> cadastrosDados = new ArrayList();
-        cadastrosDados.add("Nome: ");
-        cadastrosDados.add("Cep: ");
-        cadastrosDados.add("Rua: ");
-        cadastrosDados.add("Numero: ");
-        cadastrosDados.add("Complemento: ");
-        cadastrosDados.add("Bairro: ");
-        cadastrosDados.add("Cidade: ");
-        cadastrosDados.add("Estado: ");
-        cadastrosDados.add("Telefone: ");
-
 
         dadosPessoais.add("Valteir");
         dadosPessoais.add("999-999");
@@ -31,9 +20,9 @@ public class Main {
         dadosPessoais.set(0, "Valteir Eleuterio Alves");
         dadosPessoais.remove(8);
 
-        for (int i = 0; i < dadosPessoais.size(); i++) {
-            System.out.println(cadastrosDados.get(i) + dadosPessoais.get(i));
-        }
+        dadosPessoais.stream().forEach((item)->{
+            System.out.println(item);
+        });
     }
 }
 
